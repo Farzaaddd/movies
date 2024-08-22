621692862e11c8ee844f42fd5b1a60ceeddb0f1d
Farzad Solaimani <farzadsolaimanii@gmail.com>
2024-08-23 03:11:36 +0400

    footer has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HashRouter as Routers } from 'react-router-dom';
 import Router from "./routes/Router"
+import Footer from './layout/Footer';
 
 const App = () => {
 
   return (
     <Routers>
       <Router/>
+      <Footer/>
     </Routers>
   )
 }
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+        <p> Designed and developed by <a href="https://github.com/Farzaaddd/"> Farzaaddd </a> ❤ </p>
+    </div>
+  )
+}
+
+export default Footer
